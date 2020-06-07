6749c54734e9277abccbd4fc8038df3f2a444347
ABearish <Anton.Berisha01@gmail.com>
2020-06-07 16:45:26 -0400

    updated yml

--- database/MongoDB/index.js
+++ database/MongoDB/index.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://mongo:27017/Reviews", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  poolSize: 5
-})
+const {MONGO_DATABASE,
+  MONGO_HOST,
+  MONGO_PASSWORD,
+  MONGO_USERNAME,
+  MONGO_PORT,
+  MONGO_OPTIONS} = require('./config');
+
+mongoose.connect(`mongodb://${MONGO_HOST}/${MONGO_DATABASE}`, MONGO_OPTIONS)
 .then(() => console.log('Connected to Mongodb'))
 .catch((err) => console.log('ERROR', err));
 
